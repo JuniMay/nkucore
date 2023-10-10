71963a32f4bf1d8a76a1ea7d6e0afd16e79b1003
Juni May <juni_may@outlook.com>
2023-10-10 17:01:46 +0800

    fix(pmm): buddy allocator

--- kern/mm/buddy_pmm.c
+++ kern/mm/buddy_pmm.c
@@ -7,11 +7,19 @@ static void buddy_init(void) {
 }
 
 static void dbg_buddy() {
-    cprintf("[dbg_buddy] order: ");
-    for (int order = BUDDY_MAX_ORDER - 1; order >= 0; order--) {
-        cprintf("%2d ", order);
-    }
-    cprintf("\n");
+    // for (int order = BUDDY_MAX_ORDER - 1; order >= 0; order--) {
+    //     // print linked list
+    //     list_entry_t *le = &buddy_zone.free_area[order].free_list;
+
+    //     cprintf("[dbg_buddy] list: %016x --> ", le);
+
+    //     while ((le = list_next(le)) != &buddy_zone.free_area[order].free_list) {
+    //         cprintf("%016lx --> ", le2page(le, page_link));
+    //     }
+
+    //     cprintf("\n");
+    // }
+
     cprintf("[dbg_buddy] block: ");
     for (int order = BUDDY_MAX_ORDER - 1; order >= 0; order--) {
         cprintf("%2d ", buddy_zone.free_area[order].nr_free / (1 << order));
@@ -32,30 +40,40 @@ static size_t nearest_order(size_t n) {
 }
 
 static void add_to_free_list(struct Page *page, size_t order) {
-    assert(order <= BUDDY_MAX_ORDER);
+    assert(order < BUDDY_MAX_ORDER);
 
     buddy_zone.free_area[order].nr_free += page->property;
 
+    // cprintf("[add_to_free_list] page: 0x%016lx, order: %d, nr_free: %d\n", page,
+    //         order, buddy_zone.free_area[order].nr_free);
+
     if (list_empty(&buddy_zone.free_area[order].free_list)) {
+        // cprintf("[add_to_free_list] list empty\n");
         // initialize the list.
         list_add(&buddy_zone.free_area[order].free_list, &(page->page_link));
     } else {
         // iterator
-        list_entry_t *list_entry = &buddy_zone.free_area[order].free_list;
+        list_entry_t *le = &buddy_zone.free_area[order].free_list;
 
-        while ((list_entry = list_next(list_entry)) !=
-               &buddy_zone.free_area[order].free_list) {
+        while ((le = list_next(le)) != &buddy_zone.free_area[order].free_list) {
+            // cprintf("[add_to_free_list] list next: %016lx, ", le);
+            // cprintf("free list: %016lx\n",
+            // &buddy_zone.free_area[order].free_list);
+
             // get the corresponding page.
-            struct Page *p = le2page(list_entry, page_link);
+            struct Page *p = le2page(le, page_link);
             // keep the list sorted.
             if (page < p) {
                 // insert before the current page.
-                list_add_before(list_entry, &(page->page_link));
+                // cprintf("[add_to_free_list] insert before: %016lx\n", p);
+                list_add_before(le, &(page->page_link));
                 break;
-            } else if (list_next(list_entry) ==
+            } else if (list_next(le) ==
                        &buddy_zone.free_area[order].free_list) {
+                // cprintf("[add_to_free_list] insert at the end\n");
                 // insert at the end of the list.
-                list_add(list_entry, &(page->page_link));
+                list_add(le, &(page->page_link));
+                break;
             }
         }
     }
@@ -74,8 +92,8 @@ static void buddy_init_memmap(struct Page *base, size_t n) {
         set_page_ref(p, 0);
     }
 
-    cprintf("[buddy_init_memmap] base: %016lx\n", page2pa(base));
-    cprintf("[buddy_init_memmap] n:    %016lx\n", n);
+    // cprintf("[buddy_init_memmap] base: 0x%016lx\n", page2pa(base));
+    // cprintf("[buddy_init_memmap] n:    0x%016lx\n", n);
 
     // Construct the zone
 
@@ -88,10 +106,10 @@ static void buddy_init_memmap(struct Page *base, size_t n) {
         size_t page_cnt = 1 << order;
         size_t block_cnt = n / page_cnt;
 
-        cprintf(
-            "[buddy_init_memmap] initializing order: %2d, page_cnt: %4lu, "
-            "block_cnt: %3lu\n",
-            order, page_cnt, block_cnt);
+        // cprintf(
+        //     "[buddy_init_memmap] initializing order: %2d, page_cnt: %4lu, "
+        //     "block_cnt: %3lu\n",
+        //     order, page_cnt, block_cnt);
 
         for (size_t i = 0; i < block_cnt; i++) {
             struct Page *page = p + i * page_cnt;
@@ -103,8 +121,6 @@ static void buddy_init_memmap(struct Page *base, size_t n) {
         n -= block_cnt * page_cnt;
         p += block_cnt * page_cnt;
     }
-
-    dbg_buddy();
 }
 
 static struct Page *buddy_alloc_pages(size_t n) {
@@ -122,20 +138,19 @@ static struct Page *buddy_alloc_pages(size_t n) {
         return NULL;
     }
 
-    cprintf("[buddy_alloc_pages] order: %d, order_min: %d\n", order, order_min);
+    // cprintf("[buddy_alloc_pages] order: %d, order_min: %d\n", order, order_min);
 
-    list_entry_t *list_entry =
-        list_next(&buddy_zone.free_area[order].free_list);
+    list_entry_t *le = list_next(&buddy_zone.free_area[order].free_list);
 
     while (order > order_min) {
         // split the page.
         size_t page_cnt = 1 << (order - 1);
 
         // remove the page from the free list.
-        list_del(list_entry);
+        list_del(le);
         buddy_zone.free_area[order].nr_free -= page_cnt << 1;
 
-        struct Page *lhs_page = le2page(list_entry, page_link);
+        struct Page *lhs_page = le2page(le, page_link);
         struct Page *rhs_page = lhs_page + page_cnt;
 
         // set the property.
@@ -152,27 +167,28 @@ static struct Page *buddy_alloc_pages(size_t n) {
         add_to_free_list(rhs_page, order);
 
         // update the list entry.
-        list_entry = list_next(&buddy_zone.free_area[order].free_list);
+        le = list_next(&buddy_zone.free_area[order].free_list);
+
+        assert(le != &buddy_zone.free_area[order].free_list);
     }
 
     // update the number of free pages.
-    struct Page *page = le2page(list_entry, page_link);
+    struct Page *page = le2page(le, page_link);
     buddy_zone.free_area[order].nr_free -= page->property;
 
     // set the flags.
     ClearPageProperty(page);
 
     // remove the page from the free list.
-    list_del(list_entry);
+    list_del(le);
 
     return page;
 }
 
 static void buddy_free_pages(struct Page *base, size_t n) {
-
     int order = (int)nearest_order(n);
 
-    cprintf("[buddy_free_pages] n: %d, order: %d\n", n, order);
+    // cprintf("[buddy_free_pages] n: %d, order: %d\n", n, order);
 
     n = 1 << order;
 
@@ -188,49 +204,63 @@ static void buddy_free_pages(struct Page *base, size_t n) {
 
     struct Page *page = base;
 
+    add_to_free_list(page, order);
+
     while (order < BUDDY_MAX_ORDER - 1) {
-        // check if this can be combined.
+        // check if this can be combined and hoisted.
 
-        list_entry_t *list_entry = list_prev(&(page->page_link));
+        list_entry_t *le = list_prev(&(page->page_link));
 
-        if (list_entry != &(buddy_zone.free_area[order].free_list)) {
-            struct Page *prev_page = le2page(list_entry, page_link);
+        if (le != &(buddy_zone.free_area[order].free_list)) {
+            struct Page *prev_page = le2page(le, page_link);
+
             if (prev_page + prev_page->property == page) {
                 // combine.
-                list_del(list_entry);
-                buddy_zone.free_area[order].nr_free -= prev_page->property;
+                list_del(le);
+                // cprintf("[buddy_free_pages] del: 0x%016lx, order: %d\n",
+                //         le2page(le, page_link), order);
+                list_del(&(page->page_link));
+                // cprintf("[buddy_free_pages] del: 0x%016lx, order: %d\n", page,
+                //         order);
+
+                buddy_zone.free_area[order].nr_free -= 1 << (order + 1);
                 prev_page->property <<= 1;
                 // set the flags.
                 ClearPageProperty(page);
                 // add prev_page to the free list in the larger order.
                 order++;
                 page = prev_page;
+                add_to_free_list(page, order);
                 continue;
             }
         }
 
-        list_entry = list_next(&(page->page_link));
+        le = list_next(&(page->page_link));
 
-        if (list_entry != &(buddy_zone.free_area[order].free_list)) {
-            struct Page *next_page = le2page(list_entry, page_link);
+        if (le != &(buddy_zone.free_area[order].free_list)) {
+            struct Page *next_page = le2page(le, page_link);
+
             if (page + page->property == next_page) {
                 // combine.
-                list_del(list_entry);
-                buddy_zone.free_area[order].nr_free -= next_page->property;
+                list_del(le);
+                // cprintf("[buddy_free_pages] del: 0x%016lx, order: %d\n",
+                //         le2page(le, page_link), order);
+                list_del(&(page->page_link));
+                // cprintf("[buddy_free_pages] del: 0x%016lx, order: %d\n", page,
+                //         order);
+                buddy_zone.free_area[order].nr_free -= 1 << (order + 1);
                 page->property <<= 1;
                 // set the flags.
                 ClearPageProperty(next_page);
                 // add page to the free list in the larger order.
                 order++;
+                add_to_free_list(page, order);
                 continue;
             }
         }
 
         break;
     }
-
-    // add to the free list.
-    add_to_free_list(page, order);
 }
 
 static size_t buddy_nr_free_pages(void) {
@@ -242,22 +272,73 @@ static size_t buddy_nr_free_pages(void) {
 }
 
 static void buddy_check(void) {
-    struct Page *p0 = alloc_pages(3);
-    dbg_buddy();
-    struct Page *p1 = alloc_pages(1024);
-    dbg_buddy();
-    struct Page *p2 = alloc_pages(513);
-    dbg_buddy();
-    struct Page *p3 = alloc_pages(65);
+    struct Page *p0, *p1, *p2, *p3, *p4, *p5, *p6, *p7, *p8;
+
+    size_t initial_nr_free_pages = nr_free_pages();
+
+    size_t initial_nr_free_array[BUDDY_MAX_ORDER] = {0};
+    for (int order = 0; order < BUDDY_MAX_ORDER; order++) {
+        initial_nr_free_array[order] = buddy_zone.free_area[order].nr_free;
+    }
+
     dbg_buddy();
 
-    free_pages(p0, 3);
+    p0 = alloc_pages(1);
+    p1 = alloc_pages(1);
+    p2 = alloc_pages(1);
+    p3 = alloc_pages(1);
+    p4 = alloc_pages(1);
+    p5 = alloc_pages(1);
+    p6 = alloc_pages(1);
+    p7 = alloc_pages(1);
+    p8 = alloc_pages(1);
+
+    assert(p0 != NULL);
+    assert(p1 != NULL);
+    assert(p2 != NULL);
+    assert(p3 != NULL);
+    assert(p4 != NULL);
+    assert(p5 != NULL);
+    assert(p6 != NULL);
+    assert(p7 != NULL);
+    assert(p8 != NULL);
+
+    assert(nr_free_pages() == initial_nr_free_pages - 9);
+
     dbg_buddy();
-    free_pages(p1, 1024);
+
+    free_pages(p7, 1);
+    free_pages(p8, 1);
+    free_pages(p2, 1);
+    free_pages(p5, 1);
+    free_pages(p6, 1);
+    free_pages(p1, 1);
+    free_pages(p4, 1);
+    free_pages(p0, 1);
+    free_pages(p3, 1);
+
+    assert(nr_free_pages() == initial_nr_free_pages);
+
     dbg_buddy();
-    free_pages(p2, 513);
+
+    p0 = alloc_pages(512);
+
+
+    assert(p0 != NULL);
+    assert(p0->property == 512);
+
+    p1 = alloc_pages(513);
+
+    assert(p1 != NULL);
+    assert(p1->property == 1024);
+
     dbg_buddy();
-    free_pages(p3, 65);
+
+    free_pages(p0, 512);
+    free_pages(p1, 513);
+
+    assert(nr_free_pages() == initial_nr_free_pages);
+
     dbg_buddy();
 }
 
